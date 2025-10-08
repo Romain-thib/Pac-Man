8447a3a9c603f4f0d995e861dc72df8ffd08a842
Cohet Simon <simon_cohet@ens.univ-artois.fr>
2025-10-08 14:11:26 +0200

    Creation de la classe CardGeneratorDecorated

---
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/map/CardGeneratorDecorated.java
@@ -0,0 +1,32 @@
+/**
+ * Ce fichier fait partie du projet projet-2025-2026.
+ *
+ * (c) 2025 simon.cohet
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.r304.pacman.model.map;
+
+
+/**
+ * Le type CardGeneratorDecorated
+ *
+ * @author simon.cohet
+ *
+ * @version 0.1.0
+ */
+public class CardGeneratorDecorated implements ICardGenerator {
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.r304.pacman.model.map.ICardGenerator#generate(int, int)
+     */
+    @Override
+    public GameMap generate(int height, int width) {
+        // TODO Auto-generated method stub.
+        return null;
+    }
+
+}
+
