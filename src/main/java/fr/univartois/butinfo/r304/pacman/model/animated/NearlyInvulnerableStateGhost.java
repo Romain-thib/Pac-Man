05bc9f3bf077cf3735fca07501a68ae1560a9f49
Cohet Simon <simon_cohet@ens.univ-artois.fr>
2025-11-10 14:41:28 +0100

    Ajout de l'état de presque invulnérabilité #18

---
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/NearlyInvulnerableStateGhost.java
@@ -0,0 +1,90 @@
+/**
+ * Ce fichier fait partie du projet projet-2025-2026.
+ *
+ * (c) 2025 simon.cohet
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.r304.pacman.model.animated;
+
+import fr.univartois.butinfo.r304.pacman.model.PacmanGame;
+import fr.univartois.butinfo.r304.pacman.view.Sprite;
+import fr.univartois.butinfo.r304.pacman.view.SpriteStore;
+
+
+/**
+ * Le type NearlyInvulnerableStateGhost
+ *
+ * @author simon.cohet
+ *
+ * @version 0.1.0
+ */
+public class NearlyInvulnerableStateGhost implements IStateGhost {
+    /**
+     * L'attribut temps représente le temps restant avant de redevenir vulnerable
+     */
+    private double time = 5000;
+    
+    /**
+     * Les sprites du fantômes dans cet état
+     */
+    private Sprite spritesGhost = null;
+    
+    /**
+     * Définit si le fantôme à été mangé par pacman ou non
+     */
+    private boolean eaten = false;
+    
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.r304.pacman.model.animated.IStateGhost#moveState(fr.univartois.butinfo.r304.pacman.model.animated.Ghost, long, double, fr.univartois.butinfo.r304.pacman.model.PacmanGame)
+     */
+    @Override
+    public void moveState(Ghost ghost, long delta, double speedOfGhostState, PacmanGame game) {
+        ghost.setStrategyGhost(new ChaseStrategyGhost(-speedOfGhostState));
+        time -= 1;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.r304.pacman.model.animated.IStateGhost#handleCollisionWithPacman(fr.univartois.butinfo.r304.pacman.model.animated.Ghost, fr.univartois.butinfo.r304.pacman.model.PacmanGame)
+     */
+    @Override
+    public void handleCollisionWithPacman(Ghost ghost, PacmanGame game) {
+        eaten = true;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.r304.pacman.model.animated.IStateGhost#getSpriteGhost(fr.univartois.butinfo.r304.pacman.model.animated.Ghost)
+     */
+    @Override
+    public void getSpriteGhost(Ghost ghost) {
+        if (spritesGhost == null) {
+            spritesGhost = new SpriteStore().getSprite("ghosts/hurt/1", "ghosts/hurt/2");
+        }
+        ghost.setSprite(spritesGhost);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.r304.pacman.model.animated.IStateGhost#nextState()
+     */
+    @Override
+    public IStateGhost nextState() {
+        if (eaten) {
+            return new FleeingStateGhost();
+        }
+        if (time <= 0) {
+            return null; // TODO : Remplacer par état invulnérable
+        } else {
+            return this;
+        }
+    }
+
+}
+
