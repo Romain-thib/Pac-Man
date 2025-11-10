8f628d61304b096026381692ae454e2c6d811286
Romain THIBAUT <romain_thibaut1@ens.univ-artois.fr>
2025-11-10 13:55:05 +0100

    Modification du nom de la méthode OnCollisionWith pour OnCollisionWithGost pour plus de cohérence. #22

--- src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/IStatePacman.java
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/IStatePacman.java
@@ -7,6 +7,9 @@
 
 package fr.univartois.butinfo.r304.pacman.model.animated;
 
+import fr.univartois.dpprocessor.designpatterns.state.StateDesignPattern;
+import fr.univartois.dpprocessor.designpatterns.state.StateParticipant;
+
 /**
  * Le type IStatePacman
  *
@@ -14,13 +17,14 @@ package fr.univartois.butinfo.r304.pacman.model.animated;
  *
  * @version 0.1.0
  */
+@StateDesignPattern(state = IStatePacman.class, participant = StateParticipant.INTERFACE)
 public interface IStatePacman {
    
     /**
      * @param pacman Savoir si pacman est en collision avec un fantome
      * @return IStatePacman pour gérer sa vulnerabilité
      */
-    public IStatePacman onCollisionWith(PacMan pacman);
+    public IStatePacman onCollisionWithGhost(PacMan pacman);
     
     /**
      * @param time Le temps que pacman ne peut pas rentrer en collision avec des fantômes 
